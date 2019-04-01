308e81a325a3266762fe5ff229260aa2fdfa190e
FGross <44384879+FGross3@users.noreply.github.com>
2019-04-01 13:25:42 -0700

    route skeleton

--- routes/api/artists.js
+++ routes/api/artists.js
@@ -1,16 +1,16 @@
+
 const router = require("express").Router();
-const artistController = require("../../controllers/localController");
 //const future contoller with the db queries
 
 
 router.use("/")
-    .get(localController.findAll)
-    .post(localController.create);
+    .get()
+    .post();
 
 router.use("/:id")
-    .get(localController.findById)
-    .post(localController.create)
-    .put(localController.update)
-    .delete(localController.remove);
+    .get()
+    .post()
+    .update()
+    .delete();
 
 module.exports = router;
