c6242a73b1bc1e0c43824152cdd78c055fa801c7
debarge <43295961+aodem@users.noreply.github.com>
2019-03-31 17:21:28 -0700

    api route push

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const artistRoutes = require("./artists");
+const userRoutes = require("./users");
 
-// Book routes
+// artist routes
 router.use("/artists", artistRoutes);
+
+//user routes
+router.use("/users", userRoutes)
 
 module.exports = router;
