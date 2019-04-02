20f18eedf0207faa7a0229c539cd6d254092f92f
FGross <44384879+FGross3@users.noreply.github.com>
2019-04-01 20:45:52 -0700

    tweaked router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import NavTabs from "./components/NavTabs";
+import Splash from "./components/pages/Splash";
 import SignIn from "./components/pages/SignIn";
 import Profile from "./components/pages/Profile";
 import Search from "./components/pages/Search";
@@ -10,7 +11,8 @@ function App() {
     <Router>
       <div>
         {/* <NavTabs /> */}
-        <Route exact path="/" component={SignIn} />
+        <Route exact path ="/" component ={Splash} />
+        <Route exact path="/signin" component={SignIn} />
         <Route exact path="/profile" component={Profile} />
         <Route exact path="/search" component={Search} />
       </div>
