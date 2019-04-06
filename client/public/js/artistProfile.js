4cd192b7a93cfe9ecc360bc7db968b1e2be7f466
debarge <43295961+aodem@users.noreply.github.com>
2019-04-05 19:26:48 -0700

    more media upload

--- client/public/js/artistProfile.js
+++ client/public/js/artistProfile.js
@@ -1,23 +1,37 @@
-// firsebase connection & dependency
 const firebase = require('firebase');
-// Set the configuration for your app
-const app = firebase.initializeApp({
+
+// Initialize Firebase
+const config = {
   apiKey: "AIzaSyDXuppBrOzR5S6jEG0-i1YtXNmVWA0mAhI",
   authDomain: "whatslocal-3cb63.firebaseapp.com",
   databaseURL: "https://whatslocal-3cb63.firebaseio.com",
   projectId: "whatslocal-3cb63",
-  storageBucket: "whatslocal-3cb63.appspot.com",
+  storageBucket: "whatslocal-3cb63",
   messagingSenderId: "451310734611"
-});
+};
+firebase.initializeApp(config);
+
 // Get a reference to the storage service, which is used to create references in your storage bucket
-const storage = firebase.storage("gs://whatslocal-3cb63");
-// Create a storage reference from our storage service
-const storageRef = storage.ref();
+//const storage = firebase.storage("gs://whatslocal-3cb63");
 
-
-// Jquery code for retrieving the file from user upload 
+// Jquery code for retrieving the file from user upload
 $(() => {
-    $().click(() => {
-        const file = $('[type=file]').files[0];
-    })
-})
+  const uploader = $("#uploader");
+
+  $("#fileButton").on("change", function(e) {
+    console.log("uploaded");
+
+    // grabbing the file
+    const file = e.target.files[0];
+    // creating our strage reference
+    const storageRef = firebase.storage().ref("photos/" + file.name);
+    // upload file
+    const task = storageRef.put(file);
+    //  update progress bar
+    task.on("state_changed", function progress(snapshot) {
+      const percentageTransferred =
+        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      uploader.value = percentageTransferred;
+    });
+  });
+});
