34ab33dfc7c79e30146e5aa08c034061dda36885
Jorge Arturo Bradic <abradic95@email.arizona.edu>
2019-04-10 17:59:52 -0700

    test sync

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,99 +3,86 @@ import "./style.css";
 import API from "../../utils/API";
 import ProfilePicture from "../ProfilePicture";
 import Bio from "../components/Bio";
-import MediaClips from "../components/MediaClips"
+import MediaClips from "../components/MediaClips";
 import Col from "../components/Col";
 import Container from "../components/Container";
 import Row from "../components/Row";
 
-
 class Profile extends Component {
-    state = {
-        firstName: "",
-        lastName: "",
-        socialMediaHandles: "",
-        bio: "",
-        profilePicture: "",
-        mediaClips: ""
+  state = {
+    firstName: "",
+    lastName: "",
+    socialMediaHandles: "",
+    bio: "",
+    profilePicture: "",
+    mediaClips: ""
+  };
+  componentDidMount() {
+    this.loadProfile();
+  }
 
-
-    };
-    componentDidMount() {
-        this.loadProfile();
-    }
-
-    loadProfile = () => {
-        API.getProfile().then(res => this.setState({
-            firstName: res.data,
-            lastName: "",
-            socialMediaHandles: "",
-            bio: "",
-            profilePicture: "",
-            mediaClips: ""
+  loadProfile = () => {
+    API.getProfile()
+      .then(res =>
+        this.setState({
+          firstName: res.data,
+          lastName: "",
+          socialMediaHandles: "",
+          bio: "",
+          profilePicture: "",
+          mediaClips: ""
         })
-        ).catch(err => console.log(err));
-    };
+      )
+      .catch(err => console.log(err));
+  };
 
-
-    render() {
-        return (
-            <Container fluid>
-           
-        
-                <Row>
-                    <Col size="md-6">
-                        <h1>Profile Picture</h1>
-                        <img>
-                            {this.state.profilePicture}</img>
-                    </Col>
-                    <Col size="md-6">
-                        <Row>
-                            <h1>Name</h1>
-                            <h1> {this.state.firstName}
-                                {this.state.lastName}
-                            </h1>
-                        </Row>
-                        <Row>
-                            {this.state.socialMediaHandles}
-                        </Row>
-                        <Row>
-                            <h1>Bio</h1>
-                            <div>
-                                {this.state.bio}
-                            </div>
-                        </Row>
-                    </Col>
-                </Row>
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <h1>Profile Picture</h1>
+            <img>{this.state.profilePicture}</img>
+          </Col>
+          <Col size="md-6">
             <Row>
-                <Col size="md-4">
-                    <div>
-                        <h1>Media 1</h1>
-                        {this.state.mediaClips}
-                    </div>
-                </Col>
-                <Col size="md-4">
-                    <div>
-                        <h1>Media 2</h1>
-                        {this.state.mediaClips}
-                    </div>
-                </Col>
-                <Col size="md-4">
-                    <div>
-                        <h1>Media 3</h1>
-                        {this.state.mediaClips}
-                    </div>
-                </Col>
-
-
+              <h1>Name</h1>
+              <h1>
+                {" "}
+                {this.state.firstName}
+                {this.state.lastName}
+              </h1>
             </Row>
-
-
-
-
-            </Container >
-        )
-    }
-
+            <Row>{this.state.socialMediaHandles}</Row>
+            <Row>
+              <h1>Bio</h1>
+              <div>{this.state.bio}</div>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-4">
+            <div>
+              <h1>Media 1</h1>
+              {this.state.mediaClips}
+            </div>
+          </Col>
+          <Col size="md-4">
+            <div>
+              <h1>Media 2</h1>
+              {this.state.mediaClips}
+            </div>
+          </Col>
+          <Col size="md-4">
+            <div>
+              <h1>Media 3</h1>
+              {this.state.mediaClips}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default Profile;
