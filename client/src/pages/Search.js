6e5dd5acf7cc0a3e018bddc5b936d79e2f99ea4f
Jack Pearson <gasca.anthony@gmail.com>
2019-04-13 13:48:05 -0700

    updating components and styles

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import SearchJumbo from "../components/SearchJumbo";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
@@ -21,14 +20,11 @@ import "./Search.css";
 //   export default Search;
 //   render(<Search />);
 
-=======
-import SearchJumbo from "../SearchJumbo";
->>>>>>> a18170bb39771eecffdfb00859b00f14da0246bd
 
 function Search() {
   return (
     <div>
-      <div class="jumbotron jumbotron-fluid">
+      <div class="jumbotron jumbotron-fluid search-jumbo">
         <h1 class="display-4">Search by</h1>
       </div>
       <SearchJumbo />
