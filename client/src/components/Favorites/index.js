e6781fecac5b99e8826874135ee47499b5afb08b
Jack Pearson <gasca.anthony@gmail.com>
2019-04-12 23:47:34 -0700

    added favorites component

--- client/src/components/Favorites/index.js
+++ client/src/components/Favorites/index.js
@@ -3,7 +3,24 @@ import "./style.css";
 
 function Favorites(props){
 
-    
+    return(
+        <div>
+            <Container>
+                <Row>
+                    <Col size="md-12">
+                        <h2>Favorite Artists</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-12">
+                        <Card>
+                            
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 
 
 }
