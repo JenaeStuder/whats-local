e9c278ff22b837482a87a7f603a17d3bc68eada9
Jack Pearson <gasca.anthony@gmail.com>
2019-04-13 01:31:42 -0700

    fixing User page

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -1,84 +1,82 @@
 import React, { Component } from "react";
-//import API from "../utils/API";
+import API from "../utils/API";
 import Col from "../components/Col";
 import Container from "../components/Container";
 import Row from "../components/Row";
-import Header from "../components/Header";
-import Navbar from "../components/Navbar";
-import Favorites from "../components/Favorites";
-import waves from "../images/vertical-waves.png";
-import "./User.css";
-//import Favorites from "../components/favorites";
+import Favorites from "../components/favorites";
 
 
 
 
-class normalUser extends Component {
+class Profile extends Component {
     state = {
-        firstName: "Vincent",
-        lastName: "van Gogh",
-        favorites: " "
+        firstName: "",
+        lastName: "",
+        favorites:""
 
 
     };
-    // componentDidMount() {
-    //     this.loadProfile();
-    // }
+    componentDidMount() {
+        this.loadProfile();
+    }
 
-    // loadProfile = () => {
-    //     API.getProfile().then(res => this.setState({
-    //         firstName: res.data,
-    //         lastName: "",
-    //         favorites:"",
-    //     })
-    //     ).catch(err => console.log(err));
-    // };
+    loadProfile = () => {
+        API.getProfile().then(res => this.setState({
+            firstName: res.data,
+            lastName: "",
+            favorites:"",
+        })
+        ).catch(err => console.log(err));
+    };
 
 
     render() {
         return (
-        <div>
             <Container fluid>
-
+           
+        
                 <Row>
-                    <Col size="md-12" id="header">
-                        <Header />
-                        <h1 id="profileName"> {this.state.firstName} {this.state.lastName}</h1>
+                    <Col size="md-6">
+                    
                     </Col>
-            
-                    {/* <Col size="md-6">
+                    <Col size="md-6">
                         <Row>
                             <h1>Name</h1>
-
+                            <h1> {this.state.firstName}
+                                {this.state.lastName}
+                            </h1>
                         </Row>
                         <Row>
-
+                            
                         </Row>
                         <Row>
-
+                            
                         </Row>
-                    </Col> */}
-                </Row>
-        </Container>
-        <Container fluid>
-                <Row>
-                    <Col size="md-12">
-                        <Favorites/>
                     </Col>
                 </Row>
-        </Container>
-        <Container fluid>
-                <Row>
-                    <Col size="md-12" id="navbar">
-                        <Navbar/>
-                    </Col>
+            <Row>
+                <Col size="md-4">
+                
+                </Col>
+                <Col size="md-4">
+                
+                    
+                </Col>
+                <Col size="md-4">
+                
+                </Col>
 
-                </Row>
-        </Container >
-        </div>
+
+            </Row>
+
+
+
+
+            </Container >
         )
     }
 
 }
 
-export default normalUser;
+export default Profile;
+
