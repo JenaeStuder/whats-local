645f066bc0898c65594f48847a8dad8fecec2d52
FGross <44384879+FGross3@users.noreply.github.com>
2019-04-20 12:53:48 -0700

    added more pieces

--- server.js
+++ server.js
@@ -2,34 +2,34 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const routes = require("./routes");
+const users = require("./routes/api/users");
+
 const app = express();
-const PORT = process.env.PORT || 3001; 
 
 // Bodyparser middleware
-app.use(bodyParser.urlencoded({
-    extended: true
+app.use(
+  bodyParser.urlencoded({
+    extended: false
   })
 );
-app.use(express.json())
 app.use(bodyParser.json());
-// Routes
-app.use(routes);
-
-//DB Config
-//const db = require("./config/keys").mongoURI;
-//Connect to MongoDB
-db = process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist";
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
 mongoose
-  .connect(db,{ useNewUrlParser: true })
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
-
 // Passport middleware
-//app.use(passport.initialize());
+app.use(passport.initialize());
 // Passport config
-//require("./config/passport")(passport);
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 
-// process.env.port is Heroku's port if you choose to deploy the app there
-app.listen(PORT, () => console.log(`🌎  ==> API Server now listening on PORT ${PORT} !`));
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.listen(port, () => console.log(`🌎  ==> Server now listening on PORT ${port} !`));
