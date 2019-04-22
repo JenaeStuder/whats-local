e12c9309ed5ce8d3b8846c489490afac126ce0e5
Jorge Arturo Bradic <abradic95@email.arizona.edu>
2019-04-22 00:39:05 -0700

    server.js

--- server.js
+++ server.js
@@ -17,10 +17,7 @@ app.use(bodyParser.json());
 const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
@@ -31,6 +28,8 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 // process.env.port is Heroku's port if you choose to deploy the app there
-app.listen(port, () => console.log(`🌎  ==> API Server now listening on PORT ${port} !`));
+app.listen(port, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${port} !`)
+);
