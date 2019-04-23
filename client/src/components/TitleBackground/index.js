79ce16b57c3640d8692f1d4fb9105155cc2ed21f
Jenae <Jenaenay15@yahoo.com>
2019-04-22 21:30:16 -0700

    made styling adjustments

--- client/src/components/TitleBackground/index.js
+++ client/src/components/TitleBackground/index.js
@@ -10,20 +10,36 @@ function TitleBackground() {
       <Login />
       <div className="jumbotron jumbotron-fluid" id="title-jumbo">
         <div className="container">
-        <Row>
-          <Col xs={12}>
-          <h1 className="display-3">what's local</h1>
-          </Col>
+          <Row>
+            <Col xs={12}>
+              <h1 className="display-3">what's local</h1>
+            </Col>
           </Row>
           <Row>
             <Col xs={12}>
-          <p className="lead">connect | local | art</p>
-          </Col>
+              <p className="lead">connect | local | art</p>
+            </Col>
           </Row>
         </div>
       </div>
-      <Searchbar />
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+
+
+    <div id="styling">
+      <Row >
+        <Col xs={2}></Col>
+        <Col xs={8}>
+        <Searchbar />
+        </Col>
+        <Col xs={2}></Col>
+      </Row>
     </div>
+    </div>
+    
   );
 }
 
