27eb65b1dc4918ae25f7c31fc4627ce50f51dba6
Jenae Studer-Hart <43565281+naeNae15232@users.noreply.github.com>
2019-04-24 20:59:18 -0700

    Update index.js

--- client/src/components/Loader/index.js
+++ client/src/components/Loader/index.js
@@ -1,25 +1,3 @@
-import React from 'react'
-import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
 
-const Loading = () => (
-  <div>
-    <Segment>
-      <Dimmer active>
-        <Loader>Loading</Loader>
-      </Dimmer>
 
-      <Image src='/images/wireframe/short-paragraph.png' />
-    </Segment>
-
-    <Segment>
-      <Dimmer active inverted>
-        <Loader inverted>Loading</Loader>
-      </Dimmer>
-
-      <Image src='/images/wireframe/short-paragraph.png' />
-    </Segment>
-  </div>
-)
-
-export default Loading;
   
