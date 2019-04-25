6f5c8bcf2c723cb4bd49fa0f8c7351b65f8f5c38
Jack Pearson <gasca.anthony@gmail.com>
2019-04-24 18:38:54 -0700

    adding css to pages

--- client/src/components/Favorites/index.js
+++ client/src/components/Favorites/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Container from "../Container";
-import Row from "../Row";
-import Col from "../Col";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import Image from "react-bootstrap/Image";
 import "./style.css";
 
 
@@ -11,28 +13,30 @@ function Favorites(props) {
 
     return (
         <div>
-            <Container>
-                <Row>
-                    <Col size="md-12">
-                        <Card bg="light" text="white" id="content">
-                            <Card.Header id="card-header">Favorite Artists</Card.Header>
-                            <Card.Body>
-                                <Card.Title></Card.Title>
-                                <Card.Text id="card-text">
-                                    Some quick example text to build on the card title and make up the bulk
-                                    of the card's content.
-                                    ;kasd;flkjasldkfj;askdjf;laksjdf;lkjads;lkfja;lskdjfl;kjasdkfja;sdlkjfa
-                                    kasd;flkjasd;lfkja;lsdkjf;laksjdf;lkjasd;lfkja;dlskjf;ladsjfasd
-                                    asdkjfal;ksdjf;lkajsd;lfkja;sdlkjf;laksjdf;lkajsd;ljqw;neaovansvknaslkv
-                                    askdnnwOEBNVAOSIJBO;AJNSBOUIAEHW'BANSDLJKFASDJFLKJADSLKFJ;OWINVALK;SDFJASDKLJLJ;
-                                    lakjfldskjfl;kajsiubjbna'wekfnv;iueijanvnae;oivlnaoevne
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <br />
-                    </Col>
-                </Row>
-            </Container>
+            <Card bg="light" text="white" id="content">
+                <Card.Header id="favoriteArtistHeader">Favorite Artists</Card.Header>
+                <Card.Body>
+                    <Row>
+                        <Col md={12}>
+                            <ListGroup>
+                                <ListGroup.Item action href="#" id="favoriteArtist">
+                                    <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg/1024px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg" id="userPic" roundedCircle />
+                                    <h4 id="favoriteArtistName" float="left">User 1</h4>
+                                </ListGroup.Item>
+                                <ListGroup.Item action href="#" id="favoriteArtist">
+                                    <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg/1024px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg" id="userPic" width="50px" height="50px" float="left" roundedCircle />
+                                    <h4 id="favoriteArtistName" float="left">User 2</h4>
+                                </ListGroup.Item>
+                                <ListGroup.Item action href="#" id="favoriteArtist">
+                                    <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg/1024px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg" id="userPic" width="50px" height="50px" float="left" roundedCircle />
+                                    <h4 id="favoriteArtistName" float="left">User 3</h4>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                    </Row>
+                </Card.Body>
+            </Card>
+
         </div>
     );
 
--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -7,6 +7,7 @@ import Searchbar from "../components/Searchbar";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Favorites from "../components/Favorites";
+import Brand from "../components/Brand";
 import "./User.css";
 //import BackgroundImage from "../components/BackgroundImage";
 //import Favorites from "../components/favorites";
@@ -43,15 +44,43 @@ class normalUser extends Component {
 
     render() {
         return (
-            <div className= "Background">
-                <div className="userHeader">
-                        <Row>
-                            <Col size="md-12" id="header">
-                            <Header />
-                            <h1 id="profileName"> {this.state.firstName} {this.state.lastName}</h1>
-                            </Col>
-                        </Row>
-                    {/* <Row>
+
+            <div className="userHeader">
+                <Container>
+                    <Row>
+                        <Col xs={2} md={2}>
+
+                            <Brand />
+
+                        </Col>
+                        <Col xs={6} md={6} id="search">
+                            <Searchbar />
+                        </Col>
+                        <Col xs={4} id="mainPageNav">
+                            <a className="nav-item nav-link navbar-brand" id="mainPageNavItem" href="#">
+                                <i class="fas fa-heart" />
+                            </a>
+                            <a className="nav-item nav-link navbar-brand" id="mainPageNavItem" href="/edit">
+                                <i class="fas fa-edit"></i>
+                            </a>
+                            <a className="nav-item nav-link navbar-brand" id="mainPageNavItem" href="/user">
+                                <i class="fas fa-users" />
+                            </a>
+                            <a className="nav-item nav-link navbar-brand btn btn-outline-info" id="mainPageLogout" title="Logout" href="login">
+                                Logout
+                            </a>
+                        </Col>
+
+                    </Row>
+                </Container>
+                <Row>
+                    <Col md={12} id="header">
+                        <br></br>
+                        <img id="profileImage" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg/1024px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg" alt="Vincent"></img>
+                        <h1 id="profileName"> {this.state.firstName} {this.state.lastName}</h1>
+                    </Col>
+                </Row>
+                {/* <Row>
                     {this.state.loading? <Col size="md-6">
 
                         <Row>
@@ -68,27 +97,27 @@ class normalUser extends Component {
                     </Col>: null}
 
                 </Row> */}
-                <Row>
-                    <Col size="md-6">
-                        <Favorites/>
+                <Row id="primaryUserRow">
+                    <Col md={3}>
+                        <Favorites />
                     </Col>
-                    <Col size="md-6">
-                        <Create/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col size="md-6">
-                        <Suggestions/>
+                    <Col md={9}>
+                        <Create />
                     </Col>
                 </Row>
-                <Row>
-                    <Col size="md-12" id="navbar">
-                        <Navbar/>
+                <Row id="secondaryUserRow">
+                    <Col md={3}>
+                        <Suggestions />
                     </Col>
                 </Row>
+                
+                    
+                {/* <Navbar /> */}
+                    
+                
 
-                </div>
             </div>
+
 
         )
     }
