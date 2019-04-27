91d66c9c60de568f8891d2d9b2e4d96c95fc6b2c
Jenae <Jenaenay15@yahoo.com>
2019-04-27 11:06:28 -0700

    made changes

--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-import Col from "../components/Col";
-import Container from "../components/Container";
-import Row from "../components/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import Navbar from "../components/Navbar";
 import Searchbar from "../components/Searchbar";
 import Brand from "../components/Brand";
@@ -41,19 +41,19 @@ class Results extends Component {
                             </Col>
                         </Row>
                         
-                {/* {this.state.loading? <Row> */}
+                {this.state.loading?( <Row>
                     <Col size="md-6">
                         <div>
                             findArtist
-                            {/* {this.state.results} */}
+                            {this.state.results}
                         </div>
                     </Col>
                     <Col size="md-6">
                         <div>
-                            {/* {this.state.results} */}
+                            {this.state.results}
                         </div>
                     </Col>
-                {/* </Row>: Loader} */}
+                </Row>):( <h3>We're sorry, your search did not return any results. Try again later!</h3>)}
                 </div>
                 </Container>
             <Navbar />
