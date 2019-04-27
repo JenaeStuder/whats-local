f9250452d48e8b66a2e3de6e72532341256e28b5
Jorge Arturo Bradic <abradic95@email.arizona.edu>
2019-04-27 00:02:02 -0700

    User.js functionality

--- client/src/components/Favorites/index.js
+++ client/src/components/Favorites/index.js
@@ -1,35 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import Button from "../Button";
 import Container from "../Container";
 import Row from "../Row";
 import Col from "../Col";
-import Card from "react-bootstrap/Card";
 import "./style.css";
 
-function Favorites(props) {
+const Favorites = props => {
   return (
-    <div>
-      <Container>
-        <Row>
-          <Col size="md-12">
-            <div bg="light" text="white" id="content">
-              <h5 id="card-header">Favorite Artists</h5>
-
-              <p id="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-                ;kasd;flkjasldkfj;askdjf;laksjdf;lkjads;lkfja;lskdjfl;kjasdkfja;sdlkjfa
-                kasd;flkjasd;lfkja;lsdkjf;laksjdf;lkjasd;lfkja;dlskjf;ladsjfasd
-                asdkjfal;ksdjf;lkajsd;lfkja;sdlkjf;laksjdf;lkajsd;ljqw;neaovansvknaslkv
-                askdnnwOEBNVAOSIJBO;AJNSBOUIAEHW'BANSDLJKFASDJFLKJADSLKFJ;OWINVALK;SDFJASDKLJLJ;
-                lakjfldskjfl;kajsiubjbna'wekfnv;iueijanvnae;oivlnaoevne
-              </p>
-            </div>
-            <br />
-          </Col>
-        </Row>
-      </Container>
+    <div class="card text-center favorites-card">
+      <div class="card-header">
+        <h5>Favorited Artists</h5>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title" />
+        <ul class="list-group list-group-flush">{props.favorites}</ul>
+      </div>
     </div>
   );
-}
+};
 
 export default Favorites;
