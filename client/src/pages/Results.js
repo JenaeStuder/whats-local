8e8b91ede3260ee6b2edf54198aff6162514c2ab
Jorge Arturo Bradic <abradic95@email.arizona.edu>
2019-04-27 11:16:14 -0700

    added className 'search-row' to all pages

--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -9,59 +9,54 @@ import Brand from "../components/Brand";
 import Loader from "../components/Loader";
 
 class Results extends Component {
-    state = {
-        results: "",
-        loading: false
+  state = {
+    results: "",
+    loading: false
+  };
+  // componentDidMount() {
+  //     this.loadResults();
+  // }
 
-    };
-    // componentDidMount() {
-    //     this.loadResults();
-    // }
+  // loadResults = () => {
+  //     API.getResults().then(res =>
+  //         this.setState({
+  //             results: res.data,
+  //             loading: false
+  //         })
+  //     ).catch(err => console.log(err));
+  // };
 
-    // loadResults = () => {
-    //     API.getResults().then(res =>
-    //         this.setState({
-    //             results: res.data,
-    //             loading: false
-    //         })
-    //     ).catch(err => console.log(err));
-    // };
+  render(props) {
+    return (
+      <div>
+        <Container>
+          <div className="Results">
+            <Row className="search-row">
+              <Col xs={4} md={4}>
+                <Brand />
+              </Col>
+              <Col xs={8} md={8} id="search">
+                <Searchbar />
+              </Col>
+            </Row>
 
-    render(props) {
-        return (
-            <div>
-                <Container>
-           <div className="Results">
-           <Row>
-                            <Col xs={4} md={4}>
-                                <Brand />
-                            </Col>
-                            <Col xs={8} md={8} id="search">
-                                <Searchbar />
-                            </Col>
-                        </Row>
-                        
-                {/* {this.state.loading? <Row> */}
-                    <Col size="md-6">
-                        <div>
-                            findArtist
-                            {/* {this.state.results} */}
-                        </div>
-                    </Col>
-                    <Col size="md-6">
-                        <div>
-                            {/* {this.state.results} */}
-                        </div>
-                    </Col>
-                {/* </Row>: Loader} */}
-                </div>
-                </Container>
-            <Navbar />
-            
-            </div>
-            
-        )
-    }
+            {/* {this.state.loading? <Row> */}
+            <Col size="md-6">
+              <div>
+                findArtist
+                {/* {this.state.results} */}
+              </div>
+            </Col>
+            <Col size="md-6">
+              <div>{/* {this.state.results} */}</div>
+            </Col>
+            {/* </Row>: Loader} */}
+          </div>
+        </Container>
+        <Navbar />
+      </div>
+    );
+  }
 
   // loadResults = () => {
   //     API.getResults().then(res =>
