780a1fee644fe9718cd392306fac77c26372cd21
FGross3 <44384879+FGross3@users.noreply.github.com>
2019-04-27 13:49:04 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(
 );
 app.use(bodyParser.json());
 
-app.use(express.static("public"));
-
+app.use(express.static('build'));
+app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html'));
 // Routes
 // app.use("/api/users", users);
 
