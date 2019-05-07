02b132143240109d3a649351893e4c3448b8138c
Jenae Studer-Hart <43565281+naeNae15232@users.noreply.github.com>
2019-05-06 21:50:31 -0700

    Update Results.js

--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -6,11 +6,9 @@ import Row from "react-bootstrap/Row";
 import Navbar from "../components/Navbar";
 import Searchbar from "../components/Searchbar";
 import Brand from "../components/Brand";
-import Spinner from "react-bootstrap/Spinner";
-<<<<<<< HEAD
+
 import Background from "../assets/images/buildings-cars.jpg";
-=======
->>>>>>> ae697f552ff8cab6471a9743d237a0082cbdc452
+
 
 class Results extends Component {
   state = {
@@ -32,7 +30,7 @@ class Results extends Component {
 
 
   render(props) {
-<<<<<<< HEAD
+
     const style ={
       minWidth: "100vw",
       minHeight: "100vh",
@@ -42,10 +40,10 @@ class Results extends Component {
     };
     return (
       <div style={style}>
-=======
+
     return (
       <div>
->>>>>>> ae697f552ff8cab6471a9743d237a0082cbdc452
+
         <div className="row no-gutters">
           <Row className="search-row">
             <Col xs={4} md={4}>
